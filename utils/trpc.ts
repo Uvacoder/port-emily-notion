4744262bcda3f52907c260fbb10beb05460a5ab8
Emily Dong <emilyhfdong@gmail.com>
2023-01-12 17:23:15 -0500

    fix: ssr trpc config

--- utils/trpc.ts
+++ utils/trpc.ts
@@ -13,17 +13,41 @@ function getBaseUrl() {
 
 export const trpc = createTRPCNext<AppRouter>({
   config({ ctx }) {
+    if (typeof window !== "undefined") {
+      // during client requests
+      return {
+        links: [
+          httpBatchLink({
+            url: "/api/trpc",
+          }),
+        ],
+      }
+    }
     return {
       links: [
         httpBatchLink({
+          // The server needs to know your app's full url
           url: `${getBaseUrl()}/api/trpc`,
+          /**
+           * Set custom request headers on every request from tRPC
+           * @link https://trpc.io/docs/v10/header
+           */
+          headers() {
+            if (ctx?.req) {
+              // To use SSR properly, you need to forward the client's headers to the server
+              // This is so you can pass through things like cookies when we're server-side rendering
+              // If you're using Node 18, omit the "connection" header
+              const { connection: _connection, ...headers } = ctx.req.headers
+              return {
+                ...headers,
+                // Optional: inform server that it's an SSR request
+                "x-ssr": "1",
+              }
+            }
+            return {}
+          },
         }),
       ],
-      queryClientConfig: {
-        defaultOptions: {
-          queries: { staleTime: Infinity, refetchOnWindowFocus: false },
-        },
-      },
     }
   },
   ssr: true,
