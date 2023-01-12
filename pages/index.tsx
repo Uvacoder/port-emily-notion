69107c1732ac2eae70b03add4254d3993cea3b3f
Emily Dong <emilyhfdong@gmail.com>
2023-01-12 15:29:10 -0500

    feat: add request for projects

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,13 @@
 import { trpc } from "../utils/trpc"
 
 export default function IndexPage() {
-  const hello = trpc.hello.useQuery({ text: "client" })
+  const hello = trpc.projects.useQuery()
   if (!hello.data) {
     return <div>Loading...</div>
   }
   return (
     <div>
-      <p>{hello.data.greeting}</p>
+      <p>{hello.data.length}</p>
     </div>
   )
 }
--- server/routers/_app.ts
+++ server/routers/_app.ts
@@ -1,18 +1,47 @@
 import { z } from "zod"
 import { procedure, router } from "../trpc"
+import { Client } from "@notionhq/client"
+import { NotionProject } from "../types"
+
+const notion = new Client({
+  auth: process.env.NOTION_TOKEN,
+})
 
 export const appRouter = router({
-  hello: procedure
-    .input(
-      z.object({
-        text: z.string(),
-      })
-    )
-    .query(({ input }) => {
-      return {
-        greeting: `hello ${input.text}`,
-      }
-    }),
+  projects: procedure.query(async () => {
+    const database = await notion.databases.query({
+      database_id: process.env.NOTION_DATABASE_ID || "",
+    })
+    const notionProjects = database.results as NotionProject[]
+
+    const projects = notionProjects
+      .filter(
+        (p) =>
+          p.properties["background colour"].rich_text.length &&
+          !p.properties.hide.checkbox
+      )
+      .sort(
+        (a, b) =>
+          new Date(b.properties.date.date.start).getTime() -
+          new Date(a.properties.date.date.start).getTime()
+      )
+      .map((p) => ({
+        id: p.id,
+        emoji: p.icon.emoji,
+        backgroundColor:
+          p.properties["background colour"].rich_text[0].plain_text,
+        name: p.properties.name.title[0].plain_text,
+        tech: p.properties.tech.multi_select,
+        url: p.properties.url.url,
+        dateCreated: p.properties.date.date.start,
+        screenshots: p.properties.screenshot.files.map((file) => file.file.url),
+        github: p.properties["github link"].url,
+        summary: p.properties.summary.rich_text[0]?.plain_text,
+        motivation: p.properties.motivation.rich_text[0]?.plain_text,
+      }))
+
+    return projects
+  }),
 })
 
 export type AppRouter = typeof appRouter
